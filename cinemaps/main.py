415f00d2c4d3cf6e6ed7bbaa159f7c617bdb4340
Fernando Freitas <freitax.li@gmail.com>
2024-11-20 11:07:39 -0300

    adicionar rotas iniciais de exemplo

--- cinemaps/main.py
+++ cinemaps/main.py
@@ -1,10 +1,36 @@
-from flask import app, render_template, request, make_response, Flask
+from flask import app, render_template, request, make_response, Flask, redirect, url_for
 
 app = Flask("cinemaps", template_folder="../templates")
 
 @app.route("/")
-def index() -> None:
+def index():
     return render_template("index.html")
+
+
+@app.route("/cinemas/<int:cinema>")
+def cinema(cinema: int):
+    return "Cinema {}".format(cinema)
+
+
+@app.route("/cinemas")
+def cinemas():
+    return "Cinemas"
+
+
+@app.route("/filmes")
+def filmes():
+    return "Filmes"
+
+
+@app.route("/filmes/<int:filme>")
+def filme(filme: int):
+    return "Filme {}".format(filme)
+
+
+@app.errorhandler(404)
+def nao_encontrado(e):
+    return "A página {} não foi encontrada! Voltar para a principal <a href=\"/\">por aqui</a>".format(request.path), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
