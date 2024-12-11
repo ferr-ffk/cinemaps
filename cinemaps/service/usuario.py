c9eae09992af1b57fa4a170f2042d5eac288a16b
Fernando Freitas <freitax.li@gmail.com>
2024-12-11 10:17:28 -0300

    criar classe para manuseio de usuário no banco

---
+++ cinemaps/service/usuario.py
@@ -0,0 +1,8 @@
+from util.sql import *
+
+class UsuarioService:
+    def criar_usuario(u: dict) -> None:
+        insert_into_tabela(u, 'Usuario')
+
+    def read_usuarios() -> Optional[list]:
+        select_from_tabela('Usuario')
