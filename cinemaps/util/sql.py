b47601039843d3a12573991894841577eae5fd11
Fernando Freitas <freitax.li@gmail.com>
2024-12-11 08:36:05 -0300

    criar módulo de funções úteis e criar pacote de funções sql

---
+++ cinemaps/util/sql.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from mysql.connector import *
+
+
+def executar_sql(conexao: MySQLConnection, sql: str) -> Optional[list]:
+    """Executa um sql em uma conexão estabelecida.
+
+    Args:
+        conexao (MySQLConnection): A conexão que será utilizada
+        sql (str): A string do SQL a ser executado
+    """
+
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute(sql)
+
+    resultado = []
+
+    for registro in cursor:
+        resultado.append(registro)
+
+    conexao.commit()
+
+    return resultado
