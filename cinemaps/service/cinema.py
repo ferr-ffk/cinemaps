f29f750b785b4c3ff8f92d1f7c54f0e946e72ae2
Fernando Freitas <126774900+ferr-ffk@users.noreply.github.com>
2024-12-12 15:06:09 -0300

    criar função para obter um cinema

--- cinemaps/service/cinema.py
+++ cinemaps/service/cinema.py
@@ -6,3 +6,6 @@ class CinemaService:
         
     def read_cinemas() -> Optional[list]:
         return select_from_tabela('cinema')
+
+    def read_cinema(campo: str, valor: str) -> Optional[list]:
+        return select_from_tabela_por_condicao('cinema', f'WHERE {campo} = \'{valor}\'')
