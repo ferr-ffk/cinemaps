bd899e4fb5e131754c1ecb9c7532065224a2b99a
Fernando Freitas <freitax.li@gmail.com>
2024-12-17 11:36:21 -0300

    adicionar data formatada em select

--- cinemaps/service/sessao.py
+++ cinemaps/service/sessao.py
@@ -9,11 +9,17 @@ class SessaoService:
     def read_sessoes() -> Optional[list]:
         conexao = criar_conexao_padrao()
 
-        r = executar_sql(conexao, f'SELECT *, DATE_FORMAT(data_horario, \'%d/%m/%y %H:%i\') as format_data_horario from {NOME_TABELA}')
+        r = executar_sql(conexao, f'SELECT *, DATE_FORMAT(data_horario, \'%d/%m/%y às %H:%i\') as format_data_horario from {NOME_TABELA}')
     
         fechar_conexao(conexao)
 
         return r
 
     def read_sessoes_por_condicao(campo: str, valor: str) -> Optional[list]:
-        return select_from_tabela_por_condicao(NOME_TABELA, f'WHERE {campo} = \'{valor}\'')
+        conexao = criar_conexao_padrao()
+
+        r = executar_sql(conexao, f'SELECT *, DATE_FORMAT(data_horario, \'%d/%m/%y às %H:%i\') as format_data_horario from {NOME_TABELA} WHERE {campo} = \'{valor}\'')
+    
+        fechar_conexao(conexao)
+
+        return r
